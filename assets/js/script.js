4edb6b053dbae0a1b0db540436f94e8fc2cea325
Abhinav7279 <abhinavanand7279@gmail.com>
2024-10-04 01:19:29 +0530

    initial update

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Abhinav Anand";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development",  "web designing", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
