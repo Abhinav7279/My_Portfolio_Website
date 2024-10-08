640022cd232ea1852dcb6ff22c37b5dea7ba39bd
Abhinav7279 <abhinavanand7279@gmail.com>
2024-10-08 08:22:53 +0530

    initial update

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development",  "web designing", "web development"],
+    strings: ["frontend development",  "web designing", "web development","SEO"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
